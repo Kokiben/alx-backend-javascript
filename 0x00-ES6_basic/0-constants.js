a6d9bc30107d66dcf28dcafcffa3d50223e6415e
Kokiben <bennassarkaoutar@gmail.com>
2024-09-30 23:47:47 +0000

    constant

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,6 @@
+/* Disable eslint rule for unused variables */
+export default function getResponseFromAPI() {
+  return new Promise((handleSuccess, handleError) => {
+    // Implement promise logic here
+  });
+}
