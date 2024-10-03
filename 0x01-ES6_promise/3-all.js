397c2307e798557363cae7eff9f14f1f4c08c5e9
Kokiben <bennassarkaoutar@gmail.com>
2024-10-03 00:56:20 +0000

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,20 @@
-// 3-all.js
+/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // Call both functions and collect their promises
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
+  // Start both promises
+  const upload = uploadPhoto();
+  const create = createUser();
 
-  // Use Promise.all to handle both promises
-  Promise.all([photoPromise, userPromise])
-    .then(([photoResponse, userResponse]) => {
-      // Log the required information to the console
+  // Use Promise.all to handle both promises together
+  return Promise.all([upload, create])
+    .then((result) => {
+      // Destructure result for better readability
+      const [photoResponse, userResponse] = result;
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
-      // Log the error message if any promise rejects
+      // Log error message in case of failure
       console.log('Signup system offline');
     });
 }
