c73469e5184ee212385610655f631fec06bff533
Kokiben <bennassarkaoutar@gmail.com>
2024-10-04 01:22:11 +0000

    build

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,23 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft; // Use the setter for validation
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate an abstract class');
-    }
+    if (typeof sqft !== 'number') throw new Error('Square footage must be a number');
+    this._sqft = sqft;
+    this.evacuationWarningMessage(); // Ensure the subclass implements this method
   }
 
   get sqft() {
-    return this._sqft;
+    return this._sqft; // Getter for sqft
   }
 
   set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Square footage must be a number');
-    }
-    this._sqft = value;
+    if (typeof value !== 'number') throw new Error('Square footage must be a number');
+    this._sqft = value; // Setter for sqft with validation
   }
 
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    // Ensure subclasses implement this method
+    if (this.constructor.name === 'Building') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
