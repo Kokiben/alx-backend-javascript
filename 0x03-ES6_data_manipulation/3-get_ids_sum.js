97b6a1ba5214724cd4232a217cf6c1afd5cc01f0
Kokiben <bennassarkaoutar@gmail.com>
2024-10-09 00:09:06 +0000

    sum

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 // 3-get_ids_sum.js
-import getListStudents from "./0-get_list_students.js";
+import getListStudents from './0-get_list_students';
 
-export default function getStudentIdsSum(students) {
-  return students.reduce((sum, student) => sum + student.id, 0);
+export default function getStudentIdsSum(student = getListStudents()) {
+  return student.reduce((sum, student) => sum + student.id, 0);
 }
