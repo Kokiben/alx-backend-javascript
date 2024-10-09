628d9bf7b825262b3001fcabdc9420d51e9702e1
Kokiben <bennassarkaoutar@gmail.com>
2024-10-09 00:29:11 +0000

    updategrade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
 // 4-update_grade_by_city.js
-import getListStudents from './0-get_list_students';
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter(student => student.location === city)
     .map(student => {
-      const gradeInfo = newGrades.find(grade => grade.studentId === student.id);
+      const matchedGrades = newGrades.filter(gradeInfo => gradeInfo.studentId === student.id);
+      const studentGrade = matchedGrades.length ? matchedGrades[0].grade : 'N/A';
       return {
         ...student,
-        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+        grade: studentGrade,
       };
     });
 }
