8c7cf454b5e7a12a7d13d6e0e0efc55561e2494a
Kokiben <bennassarkaoutar@gmail.com>
2024-10-11 21:01:31 +0000

    task1

---
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './task_1/js/main.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: './dist'
+  }
+};
