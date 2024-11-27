61cec13e774849c5aa7667b5bfd7ab8b36c8f034
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-27 02:26:56 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,7 @@
+// Display welcome message and prompt for user input
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Listen for user input
 process.stdin.on('readable', () => {
   const userInput = process.stdin.read();
 
@@ -8,6 +10,7 @@ process.stdin.on('readable', () => {
   }
 });
 
+// Handle program exit
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
