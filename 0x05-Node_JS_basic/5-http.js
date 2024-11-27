e8e38657b06ea5867107b065cb01132e428e39a7
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-27 03:35:25 +0100

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,74 +1,57 @@
-const httpServer = require('http');
+const http = require('http');
 const fileSystem = require('fs');
-const { args } = require('process');
+const { argv } = require('process');
 
-function countStudents(fileLocation, outputStream) {
-  if (fileSystem.existsSync(fileLocation)) {
-    const fileData = fileSystem.readFileSync(fileLocation, 'utf8');
-    const studentData = [];
-    fileData.split('\n').forEach((line) => {
-      studentData.push(line.split(','));
+function countStudents(filePath, responseStream) {
+  if (fileSystem.existsSync(filePath)) {
+    const fileContent = fileSystem.readFileSync(filePath, 'utf8');
+    const parsedData = [];
+    fileContent.split('\n').forEach((line) => {
+      parsedData.push(line.split(','));
     });
-    studentData.shift(); // Remove header
-    const studentsInfo = [];
-    studentData.forEach((student) => studentsInfo.push([student[0], student[3]]));
-    
-    const departments = new Set();
-    studentsInfo.forEach((info) => departments.add(info[1]));
-    
-    const departmentCount = {};
-    departments.forEach((department) => { 
-      departmentCount[department] = 0;
-    });
-
-    studentsInfo.forEach((info) => { 
-      departmentCount[info[1]] += 1;
-    });
-
-    outputStream.write(`Number of students: ${studentData.length}\n`);
-    
-    const departmentList = [];
-    Object.keys(departmentCount).forEach((department) => {
-      departmentList.push(`Number of students in ${department}: ${departmentCount[department]}. List: ${studentsInfo.filter((student) => student[1] === department).map((student) => student[0]).join(', ')}\n`);
-    });
-
-    for (let i = 0; i < departmentList.length; i++) {
-      if (i === departmentList.length - 1) {
-        departmentList[i] = departmentList[i].replace(/(\r\n|\n|\r)/gm, '');
+    parsedData.shift();
+    const studentFieldInfo = [];
+    parsedData.forEach((line) => studentFieldInfo.push([line[0], line[3]]));
+    const uniqueFields = new Set();
+    studentFieldInfo.forEach((item) => uniqueFields.add(item[1]));
+    const fieldStudentCount = {};
+    uniqueFields.forEach((field) => { (fieldStudentCount[field] = 0); });
+    studentFieldInfo.forEach((student) => { (fieldStudentCount[student[1]] += 1); });
+    responseStream.write(`Number of students: ${parsedData.length}\n`);
+    const formattedMessages = [];
+    Object.keys(fieldStudentCount).forEach((field) => formattedMessages.push(`Number of students in ${field}: ${fieldStudentCount[field]}. List: ${studentFieldInfo.filter((student) => student[1] === field).map((student) => student[0]).join(', ')}\n`));
+   // Replacing for loop with a more explicit increment
+    for (let k = 0; k < formattedMessages.length; k++) {
+      if (k === formattedMessages.length - 1) {
+        formattedMessages[k] = formattedMessages[k].replace(/(\r\n|\n|\r)/gm, '');
       }
-      outputStream.write(departmentList[i]);
+      responseStream.write(formattedMessages[k]);
     }
-  } else {
-    throw new Error('Cannot load the database');
-  }
+  } else { throw new Error('Cannot load the database'); }
 }
 
 const hostname = 'localhost';
 const port = 1245;
 
-const app = httpServer.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+const app = http.createServer((req, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
   const { url } = req;
-
   if (url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
+    response.write('Hello Holberton School!');
+    response.end();
   }
-
   if (url === '/students') {
-    res.write('This is the list of our students\n');
+    response.write('This is the list of our students\n');
     try {
-      countStudents(args[2], res); // args[2] should be the file location passed in the command line
-      res.end();
+      countStudents(argv[2], response);
+      response.end();
     } catch (err) {
-      res.end(err.message);
+      response.end(err.message);
     }
   }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname);
 
 module.exports = app;
