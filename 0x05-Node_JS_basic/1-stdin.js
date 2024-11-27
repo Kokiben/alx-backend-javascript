a9f1c55d9a7494b009e9296383085bb936eb478e
Kokiben <bennassarkaoutar@gmail.com>
2024-11-27 01:04:08 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,12 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 // Listen for user input
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim(); // Get the user input and trim whitespace
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit(); // End the program after input is processed
+process.stdin.on('readable', () => {
+    const name = process.stdin.read(); // Read the input
+    if (name !== null) {
+        process.stdout.write(`Your name is: ${name.toString().trim()}\n`); // Output the user's name
+        process.exit(); // End the program after input is processed
+    }
 });
 
 // Handle program exit
