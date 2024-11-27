c089af5dfcbcc004c70fc40a67bc6b259621cd7c
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-27 03:52:26 +0100

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,22 +1,22 @@
-const fileSystem = require('fs'); // Renamed 'fs' to 'fileSystem'
+const fileSystem = require('fs');
 
-async function countStudents(fileLocation) { // Changed 'filePath' to 'fileLocation'
-  if (fileSystem.existsSync(fileLocation)) { // Using 'fileLocation' instead of 'filePath'
-    return new Promise((resolve, reject) => { // Added reject handler
-      fileSystem.readFile(fileLocation, 'utf8', (error, data) => { // Using 'fileLocation' instead of 'filePath'
+async function countStudents(fileLocation) {
+  if (fileSystem.existsSync(fileLocation)) {
+    return new Promise((resolve, reject) => {
+      fileSystem.readFile(fileLocation, 'utf8', (error, data) => {
         if (error) {
-          reject(Error('Cannot load the database')); // Used reject instead of throw
+          reject(Error('Cannot load the database'));
         }
-        const parsedData = []; // Renamed 'result' to 'parsedData'
+        const parsedData = [];
         data.split('\n').forEach((line) => {
-          parsedData.push(line.split(',')); // Renamed 'result' to 'parsedData'
+          parsedData.push(line.split(','));
         });
-        parsedData.shift(); // Remove header row
+        parsedData.shift();
 
-        const studentCourses = []; // Renamed 'newis' to 'studentCourses'
+        const studentCourses = [];
         parsedData.forEach((record) => studentCourses.push([record[0], record[3]]));
 
-        const uniqueCourses = new Set(); // Renamed 'fields' to 'uniqueCourses'
+        const uniqueCourses = new Set();
         studentCourses.forEach((course) => uniqueCourses.add(course[1]));
 
         const courseCount = {}; // Renamed 'final' to 'courseCount'
@@ -30,7 +30,7 @@ async function countStudents(fileLocation) { // Changed 'filePath' to 'fileLocat
           console.log(`Number of students in ${course}: ${courseCount[course]}. List: ${studentCourses.filter((student) => student[1] === course).map((student) => student[0]).join(', ')}`);
         });
 
-        resolve({ parsedData, courseCount, studentCourses }); // Resolving with renamed variables
+        resolve({ parsedData, courseCount, studentCourses });
       });
     });
   }
