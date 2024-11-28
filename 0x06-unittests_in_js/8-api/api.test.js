012331c1f00440f504a253784d370d1961cee8d8
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-29 00:53:56 +0100

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,13 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
+describe('Payment System API Integration', () => {
+  const BASE_URL = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+    request.get(`${BASE_URL}/`, (error, response, responseBody) => {
+      expect(response.statusCode).to.be.equal(200);
+      expect(responseBody).to.be.equal('Welcome to the payment system');
       done();
     });
   });
