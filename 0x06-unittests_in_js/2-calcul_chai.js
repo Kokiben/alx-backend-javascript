7471fb68eb28432b105c2d43a4587f094c6a5c8a
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-28 19:15:33 +0100

    Update 2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,14 +1,1 @@
-const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === 'DIVIDE') {
-    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-  }
-  return 0;
-};
 
-export default calculateNumber;  // Use export default for ES Modules
