e09a252e279fba0d0ee1de7c4dab5f84d4ac00dc
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-29 00:06:15 +0100

    Update 7-skip.test.js

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,35 +1,35 @@
 const { expect } = require('chai');
 
-describe('Number equality checks', () => {
-  it('Checking if 1 equals 1', () => {
+describe('Testing numbers', () => {
+  it('1 is equal to 1', () => {
     expect(1 === 1).to.be.true;
   });
 
-  it('Checking if 2 equals 2', () => {
+  it('2 is equal to 2', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it.skip('Checking if 1 equals 3', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
-  it('Checking if 3 equals 3', () => {
+  it('3 is equal to 3', () => {
     expect(3 === 3).to.be.true;
   });
 
-  it('Checking if 4 equals 4', () => {
+  it('4 is equal to 4', () => {
     expect(4 === 4).to.be.true;
   });
 
-  it('Checking if 5 equals 5', () => {
+  it('5 is equal to 5', () => {
     expect(5 === 5).to.be.true;
   });
 
-  it('Checking if 6 equals 6', () => {
+  it('6 is equal to 6', () => {
     expect(6 === 6).to.be.true;
   });
 
-  it('Checking if 7 equals 7', () => {
+  it('7 is equal to 7', () => {
     expect(7 === 7).to.be.true;
   });
 });
