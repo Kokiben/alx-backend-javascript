6fdc8bcf620fbdaa28abd737cf491dac6372c83e
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-28 04:10:42 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,24 +1,29 @@
 const assert = require('assert');
-const { it, describe } = require('mocha');
+const mocha = require('mocha');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', () => {
-    it('checking if operation is correct 1', () => {
+mocha.describe('calculateNumber', () => {
+    mocha.it('should sum two numbers', () => {
         assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
     });
-    it('checking if operation is correct 2', () => {
+
+    mocha.it('should subtract two numbers', () => {
         assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
-    it('checking if operation is correct 3', () => {
+
+    mocha.it('should divide two numbers', () => {
         assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
-    it('checking if operation is correct 4', () => {
+
+    mocha.it('should return "Error" when dividing by zero', () => {
         assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
-    it('checking correct type for operation 1', () => {
+
+    mocha.it('should return "Error" for invalid input', () => {
         assert.equal(calculateNumber(5, 1, 4), 'Error');
     });
-    it('checking correct type for operation 2', () => {
+
+    mocha.it('should return "Error" for unsupported operation', () => {
         assert.equal(calculateNumber('plus', 1, 4), 'Error');
     });
 });
