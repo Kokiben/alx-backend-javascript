4a402753d6f4358a6bc0b7dbba7e30cfede2f885
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-28 03:56:39 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,36 +1,29 @@
 const assert = require('assert');
+require('mocha');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
-    it('should return 4 when adding 1 and 3', function() {
-        assert.equal(calculateNumber('SUM', 1, 3), 4);
+describe('calculateNumber', function () {
+    it('should return 6 when summing 1.4 and 4.5', function () {
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
     });
 
-    it('should return 5 when adding 1.2 and 3.7', function() {
-        assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+    it('should return -4 when subtracting 4.5 from 1.4', function () {
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
 
-    it('should return -1 when subtracting 3 from 2', function() {
-        assert.equal(calculateNumber('SUBTRACT', 3, 2), 1);
+    it('should return 0.2 when dividing 1.4 by 4.5', function () {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
-    it('should return -1 when subtracting 3.5 from 2.5', function() {
-        assert.equal(calculateNumber('SUBTRACT', 3.5, 2.5), 1);
+    it('should return "Error" when dividing by 0', function () {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 
-    it('should return 2 when dividing 5 by 2', function() {
-        assert.equal(calculateNumber('DIVIDE', 5, 2), 2);
+    it('should return "Error" for invalid operation type', function () {
+        assert.equal(calculateNumber(5, 1, 4), 'Error'); // Invalid operation type
     });
 
-    it('should return "Error" when dividing by 0', function() {
-        assert.equal(calculateNumber('DIVIDE', 5, 0), 'Error');
-    });
-
-    it('should return 2 when dividing 5.6 by 2.4', function() {
-        assert.equal(calculateNumber('DIVIDE', 5.6, 2.4), 2);
-    });
-
-    it('should return "Error" when dividing by 0 after rounding', function() {
-        assert.equal(calculateNumber('DIVIDE', 5.1, 0.4), 'Error');
+    it('should return "Error" for unsupported operation "other"', function () {
+        assert.equal(calculateNumber('other', 1, 4), 'Error'); // Invalid operation type
     });
 });
