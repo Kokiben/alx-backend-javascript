d1a1e273181d7ff3b95d6ee5143a5d54956d88af
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-11-29 22:11:34 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,20 @@
 const http = require('http');
 
 const port = 1245;
-const HOST = 'localhost';
+const host = 'localhost';
 const app = http.createServer();
 
-app.on('request', (_, res) => {
+app.on('request', (_, rsp) => {
   const responseText = 'Hello Holberton School!';
 
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.statusCode = 200;
-  res.write(Buffer.from(responseText));
+  rsp.setHeader('Content-Type', 'text/plain');
+  rsp.setHeader('Content-Length', responseText.length);
+  rsp.statusCode = 200;
+  rsp.write(Buffer.from(responseText));
 });
 
-app.listen(port, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${port}\n`);
+app.listen(port, host, () => {
+  process.stdout.write(`Server listening at -> http://${host}:${port}\n`);
 });
 
 module.exports = app;
